fac83b99eecae26eeb222de5c493482fbf76e553
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-05-18 00:08:05 +0000

    revised scraper

--- brdwy4brkppl/brdwy4brkppl/lib/scraper.rb
+++ brdwy4brkppl/brdwy4brkppl/lib/scraper.rb
@@ -6,10 +6,10 @@ class Scraper
   def self.scrape_mainpage
 		html = open(https://broadwayforbrokepeople.com/)
    	page = Nokogiri::HTML(open(https://broadwayforbrokepeople.com/)
-		shows = page.css(“CLASS”)
-    	page.css(“CLASS ”).collect do |element|
-			 {:name => element.css(“CLASS”).text , 
-	       :discount => element.css(“CLASS”).text
+		shows = page.css(“tbody tr td.xl652338”)
+    	page.css(“tbody tr td.xl652338”).collect do |element|
+			 {:name => element.css(“tbody tr td.xl652338”).text , 
+	       :discount => element.css(“tbody tr td.xl652338”).text
 			 }
   	  end
   end
